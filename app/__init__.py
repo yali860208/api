fb359afd9234ce4d4199f1763bdf56ef7fc37826
yali860208 <yali860208@gmail.com>
2021-07-23 15:14:35 +0800

    printsuccess

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from app import model
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -13,16 +14,16 @@ def test_cost():
     result = model.sum_unblendedcost(959256351448)
     return result
 
-@app.route('/predict', methods=['POST'])
-def predict_cost_amount():
-    # 取得前端傳過來的數值
-    input_id = request.get_json()
-    output_id = {}
-    cost = model.sum_unblendedcost(int(input_id['uid_cost']))
-    amount = model.sum_usageamount(int(input_id['uid_amount']))
-    output_id['unblendedcost'] = cost
-    output_id['usageamount'] = amount
-    return output_id
+# @app.route('/predict', methods=['POST'])
+# def predict_cost_amount():
+#     # 取得前端傳過來的數值
+#     input_id = request.get_json()
+#     output_id = {}
+#     cost = model.sum_unblendedcost(int(input_id['uid_cost']))
+#     amount = model.sum_usageamount(int(input_id['uid_amount']))
+#     output_id['unblendedcost'] = cost
+#     output_id['usageamount'] = amount
+#     return output_id
 
 @app.route('/cost', methods=['POST'])
 def usageaccountid_to_unblendedcost():
--- js/index.js
+++ js/index.js
@@ -16,18 +16,42 @@ function postData(url, data) {
         .then(response => response.json()) // 輸出成 json
 }
 
-function submit(){
+function submit_cost(){
     const uid_cost = document.getElementById('uid_cost').value;
-    const uid_amount = document.getElementById('uid_amount').value;
 
     const data = {
         'uid_cost': uid_cost,
+    }
+    document.getElementById('cost_output').innerHTML=
+    'Usage Account ID：' + uid_cost + '<br>Unblended Cost:'
+
+    postData('http://192.168.39.158:3000/cost', data)
+    .then(data=>{
+        console.log(JSON.stringify(data, null, '\t'));
+
+        var mycost = JSON.stringify(data, null, '\t');
+        document.getElementById('resultcost').innerHTML=
+        '<pre>' + mycost + '</pre>';
+    })
+
+}
+
+function submit_amount(){
+    const uid_amount = document.getElementById('uid_amount').value;
+
+    const data = {
         'uid_amount': uid_amount,
     }
+    document.getElementById('amount_output').innerHTML=
+    'Usage Account ID：' + uid_amount + '<br>Usage Amount:'
 
-    postData('http://192.168.39.158:3000/predict', data)
+    postData('http://192.168.39.158:3000/amount', data)
     .then(data=>{
-        console.log(data);
+        console.log(JSON.stringify(data, null, '\t'));
+
+        var myamount = JSON.stringify(data, null, '\t');
+        document.getElementById('resultamount').innerHTML=
+        '<pre>' + myamount + '</pre>';
     })
 
 }
